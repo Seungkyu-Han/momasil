64ddfb2c21e5c1eb3584683a304a90a30bd047ca
Seungkyu-Han <trust1204@gmail.com>
2025-12-15 08:35:51 +0900

    feat: cafe application container

---
+++ cafe/cafe_application/__init__.py
@@ -0,0 +1,7 @@
+from cafe.cafe_application.services.cafe_command_service_impl import CafeCommandServiceImpl
+from cafe.cafe_application.services.cafe_query_service_impl import CafeQueryServiceImpl
+
+__all__ = [
+    "CafeCommandServiceImpl",
+    "CafeQueryServiceImpl",
+]
