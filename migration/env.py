98c2fe0ceea6c292b53d6fad32ff9bd24f670b95
Seungkyu-Han <trust1204@gmail.com>
2025-12-16 07:58:58 +0900

    feat: alembic reader

--- migration/env.py
+++ migration/env.py
@@ -1,9 +1,18 @@
+import os
+import pkgutil
+import sys
 from logging.config import fileConfig
 
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from pathlib import Path
+
+import importlib
+
+from database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +23,44 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+load_dotenv()
+
+DB_ENGINE: str = os.getenv("ALEMBIC_DB_ENGINE", "postgresql+psycopg2")
+DB_USER: str = os.getenv("DB_USER")
+DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+DB_HOST: str = os.getenv("DB_HOST")
+DB_PORT: str = os.getenv("DB_PORT")
+DB_NAME: str = os.getenv("DB_NAME")
+
+
+db_url = f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+config.set_main_option("sqlalchemy.url", db_url)
+
+
+def load_all_entities_auto():
+
+    alembic_dir = Path(__file__).resolve().parent
+
+    project_root = alembic_dir.parent
+
+    sys.path.insert(0, str(project_root))
+
+    for entities_dir in project_root.rglob("entities"):
+        for _, module_name, _ in pkgutil.iter_modules([str(entities_dir)]):
+            relative_path = entities_dir.relative_to(project_root)
+            module_path = f"{str(relative_path).replace('/', '.')}.{module_name}"
+
+            importlib.import_module(module_path)
+
+
+load_all_entities_auto()
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
