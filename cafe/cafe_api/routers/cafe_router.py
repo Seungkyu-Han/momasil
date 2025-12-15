a6f40a46b222c9526a482c896959b87328688452
Seungkyu-Han <trust1204@gmail.com>
2025-12-15 12:22:12 +0900

    feat: cafe api

--- cafe/cafe_api/routers/cafe_router.py
+++ cafe/cafe_api/routers/cafe_router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
+from cafe.cafe_api.dto.request.create_cafe_request import CreateCafeRequest
 from cafe.cafe_api.dto.resposne.cafe_list_response import CafeListResponse
+from cafe.cafe_api.dto.resposne.cafe_response import CafeResponse
+from cafe.cafe_application_container import CafeQueryServiceDep, CafeCommandServiceDep
+from cafe.cafe_core import Cafe
 
 cafe_router = APIRouter(
     prefix="/cafes",
@@ -10,7 +14,41 @@ cafe_router = APIRouter(
     path="",
     summary="카페들의 전체 목록을 조회합니다.",
 )
-async def get_cafes() -> CafeListResponse:
+async def retrieve_cafes_apo(
+        cafe_query_service: CafeQueryServiceDep,
+) -> CafeListResponse:
+
+    cafes: list[Cafe] = await cafe_query_service.find_all()
+
     return CafeListResponse(
-        cafes=[]
+        cafes=[
+            CafeResponse(
+                id=str(cafe.id_),
+                name=cafe.name,
+                img=cafe.img,
+                banner=cafe.banner,
+            ) for cafe in cafes
+        ]
+    )
+
+@cafe_router.post(
+    path="",
+    summary="새로운 카페를 등록합니다",
+)
+async def create_cafe_api(
+        create_cafe_request: CreateCafeRequest,
+        cafe_command_service: CafeCommandServiceDep,
+) -> CafeResponse:
+
+    cafe = await cafe_command_service.save(
+        name=create_cafe_request.name,
+        img=create_cafe_request.img,
+        banner=create_cafe_request.banner,
+    )
+
+    return CafeResponse(
+        id=cafe.id_,
+        name=cafe.name,
+        img=cafe.img,
+        banner=cafe.banner
     )
