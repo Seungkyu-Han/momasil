33789b42fe560982b85ab4539bf9f99508f1d126
Seungkyu-Han <trust1204@gmail.com>
2025-12-16 08:03:31 +0900

    feat: connect cafe router

---
+++ cafe/__init__.py
@@ -0,0 +1,5 @@
+from cafe.cafe_api import cafe_router
+
+__all__ = [
+    "cafe_router",
+]
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
+from cafe import cafe_router
 from lifespan import lifespan
 
 app = FastAPI(
     lifespan=lifespan,
     title="momasil",
 )
+
+app.include_router(cafe_router)
 
