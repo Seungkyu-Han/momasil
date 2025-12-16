f994e09e2edf167a42bae12b8e47861308fb91d9
Seungkyu-Han <trust1204@gmail.com>
2025-12-16 12:26:34 +0900

    feat: uow in menu module

--- menu/menu_core/__init__.py
+++ menu/menu_core/__init__.py
@@ -2,10 +2,12 @@ from menu.menu_core.domains.category import Category
 from menu.menu_core.domains.menu import Menu
 from menu.menu_core.output.repositories.category_repository import CategoryRepository
 from menu.menu_core.output.repositories.menu_repository import MenuRepository
+from menu.menu_core.output.uow.menu_uow import MenuUow
 
 __all__ = [
     "Category",
     "Menu",
     "CategoryRepository",
     "MenuRepository",
+    "MenuUow",
 ]
---
+++ menu/menu_infra/uow/sql_menu_uow.py
@@ -0,0 +1,33 @@
+from typing import Callable
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from menu.menu_core import MenuUow, CategoryRepository, MenuRepository
+
+
+class SqlCafeUow(MenuUow):
+
+    def __init__(
+            self,
+            session: AsyncSession,
+            category_repository_factory: Callable[[AsyncSession], CategoryRepository],
+            menu_repository_factory: Callable[[AsyncSession], MenuRepository],
+    ):
+        self.session = session
+        self.category_repository = category_repository_factory(session)
+        self.menu_repository = menu_repository_factory(session)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            await self.rollback()
+        else:
+            await self.commit()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
